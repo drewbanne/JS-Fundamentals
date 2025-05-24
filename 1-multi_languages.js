f00938a190ffb22d81f2461c34e11eba8933f107
drewbanne <andy.banne007@gmail.com>
2025-05-24 20:20:20 +0000

    Add 1-multi_languages.js script

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,11 @@
+// This script prints three lines to the console using console.log()
+// and avoids the use of the 'var' keyword.
+
+// Print the first line
 console.log("C is fun");
+
+// Print the second line
 console.log("Python is cool");
+
+// Print the third line
 console.log("JavaScript is amazing");
